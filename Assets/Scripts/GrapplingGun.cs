7c80942be3a5dfef911ad2dedd5c7ab10e983a45
SsbThatsMe <samuel.broms@gmail.com>
2023-04-03 19:46:35 +0200

    Swinging kind of working!
    Yay! Finally!

--- Assets/Scripts/GrapplingGun.cs
+++ Assets/Scripts/GrapplingGun.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
@@ -22,8 +23,8 @@ public class GrapplingGun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) StartGrapple();
-        else if (Input.GetKeyUp(KeyCode.Space)) StopGrapple();
+        if (Input.GetMouseButtonDown(0)) StartGrapple();
+        else if (Input.GetMouseButtonUp(0)) StopGrapple();
     }
 
     private void LateUpdate()
@@ -33,7 +34,6 @@ public class GrapplingGun : MonoBehaviour
 
     void StartGrapple()
     {
-        carController.grappling = true;
         RaycastHit hit;
         if (Physics.Raycast(gunTip.position, gunTip.forward, out hit, maxDistance, whatIsGrappleable))
         {
@@ -47,9 +47,9 @@ public class GrapplingGun : MonoBehaviour
             joint.maxDistance = distanceFromPoint * 0.8f;
             joint.minDistance = distanceFromPoint * 0.25f;
 
-            joint.spring = 4.5f;
-            joint.damper = 7f;
-            joint.massScale = 4.5f;
+            joint.spring = 1000f;
+            joint.damper = 2000f;
+            joint.massScale = 1000f;
 
             lr.positionCount = 2;
         }
@@ -65,8 +65,8 @@ public class GrapplingGun : MonoBehaviour
 
     void StopGrapple()
     {
-        carController.grappling = false;
         lr.positionCount = 0;
+        Debug.Log("ASDF");
         Destroy(joint);
     }
 }
