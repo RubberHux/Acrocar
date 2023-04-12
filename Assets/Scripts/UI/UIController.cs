c598ad2f936c085a95c0b20ca6514ddb80401f98
SsbThatsMe <samuel.broms@gmail.com>
2023-04-12 12:26:25 +0200

    Fixes for controller/keyboard menu navigation
    Irons out most of the flaws in the non-mouse navigation for the menus. Previously, if you used the mouse you would not be able to use the menus. That is now fixed. Also disables certain buttons when they should not be able to be used.

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -5,16 +5,20 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
-    [SerializeField] private GameObject pauseMenu, winMenu;
+    [SerializeField] private GameObject pauseMenu, winMenu, mainMenuButtons;
+    [SerializeField] private GameObject pauseDefualtButton, winDefualtButton, settingsDefualtButton, mainMenuDefualtButton;
     [SerializeField] private TextMeshProUGUI[] uiTimeText;
     [SerializeField] private SettingsHandler settingsHandler;
+    private EventSystem eventSystem;
     private double time;
-    private InputAction pause;
+    private InputAction pause, uiNavigate;
+    private GameObject lastObject;
     public enum GameState
     {
         MainMenu,
@@ -23,18 +27,28 @@ public class UIController : MonoBehaviour
         Settings,
         Win,
     }
-    private GameState gameState;
+    public GameState gameState { get; private set; }
 
     public void SetState(GameState newState)
     {
+        if (gameState == GameState.MainMenu)
+        {
+            foreach (Button button in mainMenuButtons.GetComponentsInChildren<Button>()) button.interactable = false;
+        }
         gameState = newState;
     }
 
     private void OnEnable()
     {
+        eventSystem = EventSystem.current;
         pause = InputHandler.playerInput.LevelInteraction.Pause;
         pause.Enable();
         pause.performed += PerformPause;
+        uiNavigate = InputHandler.playerInput.UI.Navigate;
+        uiNavigate.Enable();
+        uiNavigate.performed += UINavFix;
+        //eventSystem = gameObject.GetComponent<EventSystem>();
+
         if (SceneManager.GetActiveScene().buildIndex == 0) gameState = GameState.MainMenu;
         else gameState = GameState.Playing;
     }
@@ -47,6 +61,14 @@ public class UIController : MonoBehaviour
     private void PerformPause(InputAction.CallbackContext context)
     {
         UpdatePause();
+    }
+
+    private void UINavFix(InputAction.CallbackContext context)
+    {
+        if (eventSystem.currentSelectedGameObject == null)
+        {
+            eventSystem.SetSelectedGameObject(lastObject);
+        }
     }
 
     public void UpdatePause()
@@ -69,6 +91,21 @@ public class UIController : MonoBehaviour
         }
     }
 
+    public void BackTo(GameState state, GameObject lastSelected = null)
+    {
+        if (state == GameState.Paused)
+        {
+            pauseMenu.SetActive(true);
+            SetState(GameState.Paused);
+        }
+        if (state == GameState.MainMenu)
+        {
+            foreach(Button button in mainMenuButtons.GetComponentsInChildren<Button>()) button.interactable = true;
+            SetState(GameState.MainMenu);
+        }
+        if (lastSelected != null) eventSystem.SetSelectedGameObject(lastSelected);
+    }
+
     public void SetWin()
     {
         gameState = GameState.Win;
@@ -78,6 +115,7 @@ public class UIController : MonoBehaviour
 
     private void Update()
     {
+        if (eventSystem.currentSelectedGameObject != null) lastObject = eventSystem.currentSelectedGameObject;
         if (gameState == GameState.Playing)
         {
             time += Time.deltaTime;
