6f51fe1789fe6cb15458bea5896932ceda559907
鲸 <91104491+WhaleOMO@users.noreply.github.com>
2023-04-20 23:55:32 +0200

    (WIP) basic logic setup

--- Assets/Scripts/World Item Scripts/MovingPlatform.cs
+++ Assets/Scripts/World Item Scripts/MovingPlatform.cs
@@ -1,14 +1,13 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using JetBrains.Annotations;
-using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
-using Object = UnityEngine.Object;
 
 public class MovingPlatform : MonoBehaviour
 {
+   public float speed = 2.5f;
+   
    // Icon used in editor
    private enum IconColor
    {
@@ -22,23 +21,23 @@ public class MovingPlatform : MonoBehaviour
       Purple
    }
    
-   [SerializeField] private List<GameObject> wayPoints;
-   
+   private LinkedList<Vector3> _wayPoints;
+   private LinkedListNode<Vector3> _targetPoint;
+   private bool _inversed;
+
    private void Reset()
    {
-      if (wayPoints!=null)
-      {
-         ClearAllWayPoints();
-      }
-      
-      wayPoints = InitializeWayPoints();
+      ClearAllWayPoints();
+      InitializeWayPoints();
    }
 
    private void OnDrawGizmos()
    {
-      foreach (var wayPoint in wayPoints)
+      for (int i=0; i < transform.childCount - 1; i++)
       {
-         
+         Vector3 curPoint = transform.GetChild(i).position;
+         Vector3 nextPoint = transform.GetChild(i + 1).position;
+         Gizmos.DrawLine(curPoint, nextPoint);
       }
    }
    
@@ -54,16 +53,12 @@ public class MovingPlatform : MonoBehaviour
       }
    }
    
-   List<GameObject> InitializeWayPoints()
+   void InitializeWayPoints()
    {
-      List<GameObject> startAndEndpoints = new List<GameObject>();
-      // Create two default point and add to list
+      // Create two default start and end point
       var position = transform.position;
-      GameObject startPoint = GetNewWayPoint(position - Vector3.down * 5f, "start point", IconColor.Orange);
-      GameObject endPoint = GetNewWayPoint(position - Vector3.up * 5f, "end point", IconColor.Orange);
-      startAndEndpoints.Add(startPoint);
-      startAndEndpoints.Add(endPoint);
-      return startAndEndpoints;
+      GetNewWayPoint(position - Vector3.down * 5f, "start point", IconColor.Orange);
+      GetNewWayPoint(position - Vector3.up * 5f, "end point", IconColor.Orange);
    }
 
    GameObject GetNewWayPoint(Vector3? position, String n = "way point", IconColor color = IconColor.Teal)
@@ -76,21 +71,65 @@ public class MovingPlatform : MonoBehaviour
             parent = transform
          }
       };
-
-      newWayPoint.AddComponent<Rigidbody>();
+      
       // Add icon to way point in editor
       GUIContent iconContent = EditorGUIUtility.IconContent( $"sv_label_{(int)color}");
       EditorGUIUtility.SetIconForObject(newWayPoint, (Texture2D) iconContent.image);
       return newWayPoint;
    }
 
-   private void OnDisable()
+   private void Start()
    {
-      ClearAllWayPoints();
+      _wayPoints?.Clear();
+      _wayPoints = new LinkedList<Vector3>();
+      for (int i = transform.childCount; i > 0; --i)
+      {
+         var pointPos = transform.GetChild(0).position;
+         DestroyImmediate(transform.GetChild(0).gameObject);
+         _wayPoints.AddLast(pointPos);
+      }
+      _targetPoint = _wayPoints.First;
+   }
+
+   private void OnTriggerEnter(Collider other)
+   {
+      if (other.gameObject.CompareTag("Player"))
+      {
+         other.transform.SetParent(transform);
+      }
+   }
+
+   private void OnCollisionExit(Collision other)
+   {
+      if (other.gameObject.CompareTag("Player"))
+      {
+         other.transform.SetParent(null);
+      }
    }
 
    private void FixedUpdate()
    {
-      
+      if (_targetPoint.Value == transform.position)
+      {
+         UpdateTargetPoint();
+      }
+
+      var step = speed * Time.fixedDeltaTime;
+      transform.position = Vector3.MoveTowards(transform.position, _targetPoint.Value, step);
+   }
+
+   private void UpdateTargetPoint()
+   {
+      if (_targetPoint.Next == null)
+      {
+         _inversed = true;
+      }
+
+      if (_targetPoint.Previous == null)
+      {
+         _inversed = false;
+      }
+
+      _targetPoint = _inversed ? _targetPoint.Previous : _targetPoint.Next;
    }
 }
